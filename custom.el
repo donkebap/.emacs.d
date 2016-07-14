95d49d09bab182de72db5293bcbc86dd095ea8e4
Semih Helvaci <semih.helvaci92@gmail.com>
2016-07-14 15:31:41 +0200

    Initial commit

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (cargo magit smex monokai-theme use-package spaceline solarized-theme smartparens racer flycheck-rust drag-stuff dired+ company-racer centered-window-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(spaceline-highlight-face ((t (:foreground "yellow" :background "red")))))
---
+++ init.el
@@ -0,0 +1,3 @@
+(org-babel-load-file
+ (expand-file-name "emacs-init.org"
+                   user-emacs-directory))
