047028a9c4ed56765ba1e5da6410b4c922a81247
Semih Helvaci <semih.helvaci92@gmail.com>
2016-11-30 19:02:05 +0100

    Work/Private

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (exec-path-from-shell json-mode web-mode use-package undo-tree tide spaceline smex smartparens racer projectile monokai-theme magit js2-mode flycheck-rust flycheck-pkg-config elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo adaptive-wrap))))
+    (apropospriate-theme web-mode use-package undo-tree tide spaceline smex smartparens racer projectile monokai-theme magit json-mode js2-mode flycheck-rust flycheck-pkg-config exec-path-from-shell elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo adaptive-wrap))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
