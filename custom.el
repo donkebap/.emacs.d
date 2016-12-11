bfc70f5f853b0f573b6deef8aa2eb1208edba5b0
Semih Helvaci <semih.helvaci92@gmail.com>
2016-12-11 22:29:24 +0100

    Add helm + helm-projectile + ripgrep

--- custom.el
+++ custom.el
@@ -5,10 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (apropospriate-theme web-mode use-package undo-tree tide spaceline smex smartparens racer projectile monokai-theme magit json-mode js2-mode flycheck-rust flycheck-pkg-config exec-path-from-shell elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo adaptive-wrap))))
+    (helm-ag helm-projectile web-mode use-package undo-tree tide spaceline smex smartparens racer projectile monokai-theme magit json-mode js2-mode helm flycheck-rust flycheck-pkg-config exec-path-from-shell elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo apropospriate-theme adaptive-wrap))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fringe ((t (:background "#424242"))))
  '(spaceline-highlight-face ((t (:foreground "yellow" :background "red")))))
