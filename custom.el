fed55611e1272cac4fae0d098a40770316570d55
Semih Helvaci <semih.helvaci92@gmail.com>
2016-12-14 22:46:54 +0100

    Fixed crlf issue

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-ag helm-projectile web-mode use-package undo-tree tide spaceline smex smartparens racer projectile monokai-theme magit json-mode js2-mode helm flycheck-rust flycheck-pkg-config exec-path-from-shell elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo apropospriate-theme adaptive-wrap))))
+    (web-mode use-package undo-tree tide spaceline smex smartparens racer monokai-theme magit json-mode js2-mode helm-projectile helm-ag flycheck-rust flycheck-pkg-config exec-path-from-shell elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo apropospriate-theme adaptive-wrap))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
