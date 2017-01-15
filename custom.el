6206e1ccf6563fe04e0cb76405ec4e067f423694
Semih Helvaci <semih.helvaci92@gmail.com>
2017-01-15 15:07:42 +0100

    Latex, Lua and other things

--- custom.el
+++ custom.el
@@ -5,11 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (web-mode use-package undo-tree tide spaceline smex smartparens racer monokai-theme magit json-mode js2-mode helm-projectile helm-ag flycheck-rust flycheck-pkg-config exec-path-from-shell elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo apropospriate-theme adaptive-wrap))))
+    (web-mode use-package undo-tree tide spaceline smex smartparens racer magit lua-mode json-mode js2-mode helm-projectile helm-ag flycheck-rust flycheck-pkg-config exec-path-from-shell elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo auctex apropospriate-theme adaptive-wrap))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t (:background "#424242"))))
+ '(fringe ((t (:background "unspecified-bg"))))
  '(spaceline-highlight-face ((t (:foreground "yellow" :background "red")))))
