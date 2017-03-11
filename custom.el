2e9382c305c1b21fed9c84f55ca4aae9ec41b269
Semih Helvaci <semih.helvaci92@gmail.com>
2017-03-11 14:49:09 +0100

    Fix/Break indentation

--- custom.el
+++ custom.el
@@ -4,8 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 (quote
-		(web-mode use-package tide spaceline smex smartparens racer magit lua-mode json-mode js2-mode helm-projectile helm-ag flycheck-rust flycheck-pkg-config exec-path-from-shell evil-surround evil-nerd-commenter elm-mode drag-stuff dired+ company-c-headers centered-window-mode cargo auctex apropospriate-theme adaptive-wrap))))
+   (quote
+    (lua-mode tide exec-path-from-shell json-mode web-mode use-package undo-tree spaceline smex smartparens racer magit js2-mode helm-projectile helm-ag flycheck-rust flycheck-pkg-config expand-region drag-stuff dired+ company-c-headers centered-window-mode cargo auctex apropospriate-theme adaptive-wrap))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
