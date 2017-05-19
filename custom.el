e397988b63281200b691a73761c0ca747d3b5e4b
Semih Helvaci <semih.helvaci92@gmail.com>
2017-05-19 22:07:03 +0200

    Fix path

--- custom.el
+++ custom.el
@@ -5,11 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (lua-mode tide exec-path-from-shell json-mode web-mode use-package undo-tree spaceline smex smartparens racer magit js2-mode helm-projectile helm-ag flycheck-rust flycheck-pkg-config expand-region drag-stuff dired+ company-c-headers centered-window-mode cargo auctex apropospriate-theme adaptive-wrap))))
+    (web-mode use-package undo-tree tide spaceline smex smartparens racer magit lua-mode json-mode js2-mode helm-projectile helm-ag flycheck-rust flycheck-pkg-config expand-region exec-path-from-shell drag-stuff dired+ company-c-headers centered-window-mode cargo auctex apropospriate-theme adaptive-wrap))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t (:background "#424242"))))
  '(spaceline-highlight-face ((t (:foreground "yellow" :background "red")))))
