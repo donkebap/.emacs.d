3d81fc90bf9e2f47bebf41d6f221620d6313b7ef
Semih Helvaci <semih.helvaci92@gmail.com>
2018-01-26 16:20:48 +0100

    Add company-lsp

--- custom.el
+++ custom.el
@@ -3,12 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (creamsody-theme web-mode use-package undo-tree tide spaceline smex smartparens magit lua-mode json-mode js2-mode helm-projectile helm-gtags helm-ag flycheck-pkg-config expand-region exec-path-from-shell dumb-jump drag-stuff dired+ company-c-headers auctex apropospriate-theme adaptive-wrap))))
+    '(package-selected-packages
+	 (quote
+	     (web-mode use-package undo-tree tide spaceline smex smartparens magit lua-mode lsp-rust json-mode js2-mode helm-projectile helm-gtags helm-ag flycheck-pkg-config expand-region exec-path-from-shell dumb-jump drag-stuff dired+ creamsody-theme company-lsp company-c-headers auctex apropospriate-theme adaptive-wrap))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(spaceline-highlight-face ((t (:foreground "yellow" :background "red")))))
+ )
