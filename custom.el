bac6c9c28c3a5bec85b81f774c405049bb230c37
Semih Helvaci <semih.helvaci92@gmail.com>
2018-04-14 20:08:04 +0200

    Change font and make init file work again

--- custom.el
+++ custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-    '(package-selected-packages
-	 (quote
-	     (yaml-mode web-mode use-package undo-tree tide spaceline smex smartparens magit lua-mode lsp-rust json-mode js2-mode helm-projectile helm-gtags helm-ag flycheck-pkg-config expand-region exec-path-from-shell dumb-jump drag-stuff dired+ creamsody-theme company-lsp company-c-headers auctex apropospriate-theme adaptive-wrap))))
+ '(package-selected-packages
+   (quote
+    (yaml-mode tide json-mode js2-mode web-mode dumb-jump company-c-headers flycheck-pkg-config helm-projectile helm-ag helm projectile company flycheck magit auctex smartparens drag-stuff undo-tree expand-region smex creamsody-theme exec-path-from-shell use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
