34885716abe64b9992786b5a77d40c6fe546c4fe
Semih Helvaci <semih.helvaci92@gmail.com>
2018-07-15 01:23:18 +0200

    Cleanup and change a lot of stuff

--- custom.el
+++ custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yaml-mode tide json-mode js2-mode web-mode dumb-jump company-c-headers flycheck-pkg-config helm-projectile helm-ag helm projectile company flycheck magit auctex smartparens drag-stuff undo-tree expand-region smex creamsody-theme exec-path-from-shell use-package))))
+    '(package-selected-packages
+	 (quote
+	     (sudo-edit rainbow-delimiters rainbow-mode beacon avy swiper yaml-mode web-mode use-package tide symon spaceline smex smartparens magit json-mode js2-mode ivy helm-projectile helm-ag flycheck-pkg-config fancy-battery exwm expand-region exec-path-from-shell evil dumb-jump drag-stuff dmenu creamsody-theme company-lsp company-c-headers auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
