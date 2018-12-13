b347da8f47b1b1cc78652a11c1a3aee469841455
Semih Helvaci <semih.helvaci92@gmail.com>
2018-12-13 14:04:41 +0100

    Change rust config and glsl-mode

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
     '(package-selected-packages
 	 (quote
-	     (counsel-projectile yaml-mode use-package swiper sudo-edit spaceline smex smartparens rainbow-mode rainbow-delimiters projectile magit lsp-rust flycheck-rust exec-path-from-shell evil creamsody-theme company beacon avy auctex))))
+	     (glsl-mode yaml-mode use-package sudo-edit spaceline smex smartparens rainbow-mode rainbow-delimiters racer magit lsp-rust flycheck-rust exec-path-from-shell evil creamsody-theme counsel-projectile company beacon avy auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
