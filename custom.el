a953aaf9a2f44f1ee2d76003a66fbc0e8acac637
Semih Helvaci <semih.helvaci92@gmail.com>
2019-04-18 11:40:56 +0200

    Fix web stuff

--- custom.el
+++ custom.el
@@ -3,9 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-    '(package-selected-packages
-	 (quote
-	     (glsl-mode yaml-mode use-package sudo-edit spaceline smex smartparens rainbow-mode rainbow-delimiters racer magit lsp-rust flycheck-rust exec-path-from-shell evil creamsody-theme counsel-projectile company beacon avy auctex))))
+ '(package-selected-packages
+   (quote
+    (web-mode yaml-mode use-package tide sudo-edit spaceline smex smartparens rainbow-mode rainbow-delimiters racer magit lsp-rust glsl-mode flycheck-rust exec-path-from-shell evil creamsody-theme counsel-projectile company beacon avy auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
